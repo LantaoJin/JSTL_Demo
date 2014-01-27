2ba4a64a95e908eebf8361f19121e2187292d305
Rajan prasad upadhyay <rajanpupa@gmail.com>
2014-01-27 21:32:59 +0545

    A working version of Jsp jstl web application

---
+++ src/database/layers/DepartmentDao.java
@@ -0,0 +1,15 @@
+package database.layers;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+
+public interface DepartmentDao {
+	public void insertDepartment(Department d) throws SQLException;
+	public Department selectDepartment(Integer id)throws SQLException;
+	public void updateDepartment(Department dep)throws SQLException;
+	public void deleteDepartment(Integer id)throws SQLException;
+	public List<Department> getAllDepartments()throws SQLException;//ok
+	
+}
